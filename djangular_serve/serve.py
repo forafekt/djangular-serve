47f50d031ecd76f48a9700a5463b55812ac9e59d
forafekt <forafekt@gmail.com>
2020-09-29 18:25:22 +0200

    Add args

--- djangular_serve/serve.py
+++ djangular_serve/serve.py
@@ -3,93 +3,113 @@
 DJANGULAR SERVE MAIN
 IN DEVELOPMENT
 """""
-import os
-import re
+import argparse
 
 try:
-    from djangular_serve.management.utils import Helpers
-    from djangular_serve.management.handlers import get_img_extension, _mv, get_project_static_root, get_ng_root_path
-except NameError:
-    from .management.utils import Helpers
-    from .management.handlers import get_img_extension, _mv, get_project_static_root, get_ng_root_path
+    from djangular_serve.management.objects import AngularBuild, ng_deploy, move_js, move_css, move_img, move_all, \
+        make_directory
+    from djangular_serve.management.exceptions import ArgDoesNotExist
+except (ModuleNotFoundError, ValueError):
+    from .management.objects import AngularBuild
+    from .management.exceptions import ArgDoesNotExist
 
 
-class AngularBuild:
-    """
-    Build data
-    """
+def main():
+    """""
+    DJANGULAR SERVE args
+    """""
+    parser = argparse.ArgumentParser(
+        prog='serve',
+        description='Automate your Django / Angular projects'
+                    ' with DJANGULAR-SERVE.'
+    )
 
-    static_path = get_project_static_root()
-    ng_path = get_ng_root_path()
+    parser.add_argument(
+        '-s',
+        '--serve',
+        type=str,
+        help='Build Angular to Django static.'
+    )
 
-    ng_build = "ng build"
-    prefix_prod = "--prod --output-path {}".format(static_path)
-    prefix_hash = "--output-hashing none"
+    parser.add_argument(
+        '-mv',
+        '--move',
+        type=str,
+        help='Move files to respective directories.'
+    )
 
+    parser.add_argument(
+        '-mk',
+        '--mkdir',
+        type=str,
+        help='Make a new sub directory in your static files folder.'
+    )
+    args = parser.parse_args()
 
-class OutputDestination(AngularBuild):
-    """
-    Gather destination for static
-    """
-    b = AngularBuild
-    static_url = b.static_path
+    """""
+    Serve
+    """""
+    serve = args.serve
+    move = args.move
+    mkdir = args.mkdir
 
-    js_source = '{}/*.js'.format(static_url)
-    js_path_input = os.path.join(static_url, input(">> JS DIRNAME: "))
-    js_destination = '{}/{}'.format(static_url, _mv(files=js_source, dest=js_path_input))
-    print(js_path_input)
+    #    if not os.path.isfile(p):
+    #        print('The specified source file does not exist')
+    #        sys.exit()
 
-    css_source = '{}/*.css'.format(static_url)
-    css_path_input = os.path.join(static_url, input(">> CSS DIRNAME: "))
-    css_destination = '{}/{}'.format(static_url, _mv(files=css_source, dest=css_path_input))
-    print(css_path_input)
+    arg_is_none = ArgDoesNotExist("Argument does not exist. Did you mean...\n"
+                                  "serve -s static\n"
+                                  "serve -mv js, css, img or all\n"
+                                  "serve -mk <any-dir>\n")
+    if not args:
+        raise arg_is_none
 
-    types = r'*.ico *.jpg *.jpeg *.png *.gif *.svg'
-    file_extensions = re.findall(r'[\w\.-]+.[\w\.-]+', types.strip())
-    for fe in file_extensions:
-        img_source = '{}/{}'.format(static_url, types)
-        img_path_input = os.path.join(static_url, input(">> IMAGE DIRNAME: "))
-        img_destination = '{}/{}'.format(static_url, _mv(files=img_source, dest=img_path_input))
-        exit(img_source)
+    try:
 
-    def __init__(self):
-        self.js_output = _mv(self.js_source, self.js_destination)
-        self.css_output = _mv(self.css_source, self.css_destination)
-        self.img_output = _mv(self.img_source, self.img_destination)
+        """""
+        Serve
+        """""
+        if serve:
+            if serve == "static":
+                ng_deploy()
+            else:
+                pass
 
+        """""
+        Move
+        """""
+        if move:
+            if move == "js":
+                move_js()
+            else:
+                pass
 
-def ng_deploy():
-    """
-    Build Angular project to Django static and organise.
-    """
-    b = AngularBuild()
+        if move:
+            if move == "css":
+                move_css()
+            else:
+                pass
 
-    ng_build = b.ng_build  # ng build
-    prefix_prod = b.prefix_prod  # --prod --output-path ./../../
-    prefix_hash = b.prefix_hash  # --output-hashing none
-    ng_path = b.ng_path  # Path where Angular project is
+        if move:
+            if move == "img":
+                move_img()
+            else:
+                pass
 
-    os.chdir(ng_path)  # Automatically get Angular project without CD
+        if move:
+            if move == 'all':
+                move_all()
+        else:
+            pass
 
-    # Gather data and execute command
-    compile_ = "{} {} {}".format(ng_build, prefix_prod, prefix_hash)
-    print(">> Running", compile_, "for {}".format(ng_path))
-    command = compile_
-    process = os.system(command)
-    return process
+        if mkdir:
+            make_directory(mkdir)
+        else:
+            pass
 
-
-def static_dirs_manager():
-    """
-    Prompt manager to ask if user wants to move files to subdirectories.
-    For example: mv *.js to js folder and / or *.css to css folder.
-    """
-    print("Do you want to move files to subdirs? Y/n")
-    io = OutputDestination
-
-    return io
+    except KeyboardInterrupt:
+        print("\n => You have force cancelled the session.\n")
 
 
 if __name__ == '__main__':
-    ng_deploy()
-    static_dirs_manager()
+    main()
