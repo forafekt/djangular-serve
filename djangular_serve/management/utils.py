e2a498cfb4d2696272b74be61f02127afa32f5be
forafekt <forafekt@gmail.com>
2020-10-12 14:09:08 +0100

    Add export_settings function

--- djangular_serve/management/utils.py
+++ djangular_serve/management/utils.py
@@ -1,7 +1,6 @@
 import os
-import sys
 import time
-from pathlib import Path # noqa
+from pathlib import Path  # noqa
 
 from djangular_serve import app_settings
 
@@ -11,25 +10,30 @@ class Helpers(object):
     Mixin to get commonly used Commands
     """
 
+    def export_settings(self=None):
+        get_app = os.environ.get('DJANGO_SETTINGS_MODULE')
+        export = os.system(f'export DJANGO_SETTINGS_MODULE={get_app}')
+        return export
+
     def get_ng_root_path(self):
         """
         Angular project root path.
         :return:
         """
-        return getattr(app_settings, 'NG_ROOT_PATH', ".")
+        return getattr(app_settings, 'NG_ROOT_PATH')
 
     def get_project_static_root(self):
         """
         Find django static root
         """
-        static_path = getattr(app_settings, "STATIC_ROOT", ".")
+        static_path = getattr(app_settings, "STATIC_ROOT")
         return static_path
 
     def query_yes_no(self=None):
         """
         Ask a yes/no question via input() and return their answer.
         """
-        answer = input('Please confirm you want to continue: [y/n]')
+        answer = input('Please confirm you want to continue: [Y/n]')
         if not answer or answer[0].lower() != 'y':
             print('You chose not to continue.')
             exit(1)
